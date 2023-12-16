828cee29fbac6d65f4cd1ecea989d944f90acff4
Husseim Vargas <142638555+husseimv@users.noreply.github.com>
2023-12-16 00:13:59 +0000

    proceso de modelado

--- src/models.py
+++ src/models.py
@@ -17,3 +17,30 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    img = db.Column(db.String(), unique=True, nullable=False)
+    description = db.Column(db.String(), unique=True, nullable=False)
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    birth_year = db.Column(db.String(100), unique=True, nullable=False)
+    gender = db.Column(db.String(100), unique=True, nullable=False)
+    height = db.Column(db.String(100), unique=True, nullable=False)
+    hair_color = db.Column(db.String(100), unique=True, nullable=False)
+    eye_color = db.Column(db.String(100), unique=True, nullable=False)
+     
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    climate = db.Column(db.String(100), unique=True, nullable=False)
+    diameter = db.Column(db.String(100), unique=True, nullable=False)
+    gravity = db.Column(db.String(100), unique=True, nullable=False)
+    population = db.Column(db.String(100), unique=True, nullable=False)
+    terrain = db.Column(db.String(100), unique=True, nullable=False)
+     
